8873fd21d0456ed4c739d98b93a24973159a0c64
ert135798 <117154160+ert135798@users.noreply.github.com>
2025-08-19 14:31:14 +0800

    Update redmine_gadget.js

--- redmine_gadget.js
+++ redmine_gadget.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Redmine Custom Date Input (含SD + 實際日期 + 收合預設隱藏)
 // @namespace    http://tampermonkey.net/
-// @version      2.6.3
+// @version      2.6.4
 // @description  GITHUB版本 2.6.3 改背景設碼#CCEEFF
 // @description  (過去)Redmine 左下角輸入框，可依勾選角色與欄位類型填入欄位，支援基準日期 + 偏移，清空依角色+類別欄位，預設收合且隱藏背景與邊框
 // @match        http://*/redmine/*
